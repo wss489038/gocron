39a7e68c3f5c7a291e8b53ebff283745ddc4c7fe
gaowei <huyao9950@hotmail.com>
2022-03-27 11:49:34 +0800

    opt: package

--- package.sh
+++ package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
- 
+
 # 生成压缩包 xx.tar.gz或xx.zip
 # 使用 ./package.sh -a amd664 -p linux -v v2.0.0
- 
+
 # 任何命令返回非0值退出
 set -o errexit
 # 使用未定义的变量退出
@@ -16,12 +16,12 @@ eval $(go env)
 BINARY_NAME=''
 # main函数所在文件
 MAIN_FILE=""
- 
+
 # 提取git最新tag作为应用版本
 VERSION=''
 # 最新git commit id
 GIT_COMMIT_ID=''
- 
+
 # 外部输入的系统
 INPUT_OS=()
 # 外部输入的架构
@@ -34,7 +34,7 @@ DEFAULT_ARCH=${GOHOSTARCH}
 SUPPORT_OS=(linux darwin windows)
 # 支持的架构
 SUPPORT_ARCH=(386 amd64)
- 
+
 # 编译参数
 LDFLAGS=''
 # 需要打包的文件
@@ -43,27 +43,27 @@ INCLUDE_FILE=()
 PACKAGE_DIR=''
 # 编译文件生成目录
 BUILD_DIR=''
- 
+
 # 获取git 最新tag name
 git_latest_tag() {
     local COMMIT_ID=""
     local TAG_NAME=""
     COMMIT_ID=`git rev-list --tags --max-count=1`
     TAG_NAME=`git describe --tags "${COMMIT_ID}"`
- 
+
     echo ${TAG_NAME}
 }
- 
+
 # 获取git 最新commit id
 git_latest_commit() {
     echo "$(git rev-parse --short HEAD)"
 }
- 
+
 # 打印信息
 print_message() {
     echo "$1"
 }
- 
+
 # 打印信息后推出
 print_message_and_exit() {
     if [[ -n $1 ]]; then
@@ -71,54 +71,54 @@ print_message_and_exit() {
     fi
     exit 1
 }
- 
+
 # 设置系统、CPU架构
 set_os_arch() {
     if [[ ${#INPUT_OS[@]} = 0 ]];then
         INPUT_OS=("${DEFAULT_OS}")
     fi
- 
+
     if [[ ${#INPUT_ARCH[@]} = 0 ]];then
         INPUT_ARCH=("${DEFAULT_ARCH}")
     fi
- 
+
     for OS in "${INPUT_OS[@]}"; do
         if [[  ! "${SUPPORT_OS[*]}" =~ ${OS} ]]; then
             print_message_and_exit "不支持的系统${OS}"
         fi
     done
- 
+
     for ARCH in "${INPUT_ARCH[@]}";do
         if [[ ! "${SUPPORT_ARCH[*]}" =~ ${ARCH} ]]; then
             print_message_and_exit "不支持的CPU架构${ARCH}"
         fi
     done
 }
- 
+
 # 初始化
 init() {
     set_os_arch
- 
+
     if [[ -z "${VERSION}" ]];then
         VERSION=`git_latest_tag`
     fi
     GIT_COMMIT_ID=`git_latest_commit`
     LDFLAGS="-w -X 'main.AppVersion=${VERSION}' -X 'main.BuildDate=`date '+%Y-%m-%d %H:%M:%S'`' -X 'main.GitCommit=${GIT_COMMIT_ID}'"
- 
+
     PACKAGE_DIR=${BINARY_NAME}-package
     BUILD_DIR=${BINARY_NAME}-build
- 
+
     if [[ -d ${BUILD_DIR} ]];then
         rm -rf ${BUILD_DIR}
     fi
     if [[ -d ${PACKAGE_DIR} ]];then
         rm -rf ${PACKAGE_DIR}
     fi
- 
+
     mkdir -p ${BUILD_DIR}
     mkdir -p ${PACKAGE_DIR}
 }
- 
+
 # 编译
 build() {
     local FILENAME=''
@@ -133,11 +133,11 @@ build() {
         done
     done
 }
- 
+
 # 打包
 package_binary() {
     cd ${BUILD_DIR}
- 
+
     for OS in "${INPUT_OS[@]}";do
         for ARCH in "${INPUT_ARCH[@]}";do
         package_file ${BINARY_NAME}-${OS}-${ARCH}
@@ -148,10 +148,10 @@ package_binary() {
         fi
         done
     done
- 
+
     cd ${OLDPWD}
 }
- 
+
 # 打包文件
 package_file() {
     if [[ "${#INCLUDE_FILE[@]}" = "0" ]];then
@@ -161,14 +161,14 @@ package_file() {
             cp -r ../${item} $1
     done
 }
- 
+
 # 清理
 clean() {
     if [[ -d ${BUILD_DIR} ]];then
         rm -rf ${BUILD_DIR}
     fi
 }
- 
+
 # 运行
 run() {
     init
@@ -193,7 +193,7 @@ package_gocron_node() {
 
     run
 }
- 
+
 # p 平台 linux darwin windows
 # a 架构 386 amd64
 # v 版本号  默认取git最新tag
@@ -210,7 +210,7 @@ do
     ;;
     esac
 done
- 
+
 package_gocron
 package_gocron_node
 
